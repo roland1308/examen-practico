dbbac42475ac39e7465424f98c83384326e5a539
Acciardi Renato <acciardi.dev@gmail.com>
2023-06-14 18:25:54 +0200

    Created function in main.go

--- main.go
+++ main.go
@@ -1,4 +1,14 @@
 package main
 
+func sumInt(a int, b int) int {
+	return a + b
+}
+
+func subInt(a int, b int) int {
+	return a - b
+}
+
 func main() {
+	println(sumInt(4, 2))
+	println(subInt(4, 2))
 }
